b776f3dc404ad1fa33bc55cf22b6f9234a68d0bf
Jared <126293208+indohippi@users.noreply.github.com>
2024-01-12 15:17:05 -0600

    updates to modal

--- app/page.js
+++ app/page.js
@@ -7,39 +7,56 @@ import Menu from '../components/Menu';
 import CharacterStats from '../components/CharacterStats';
 import Modal from '../components/Modal';
 import ContactInfo from '../pages/ContactInfo';
+import Objective from '../pages/Objective';
+import ProfessionalExperience from '../pages/ProfessionalExperience';
+import Education from '../pages/Education';
+import Certifications from '../pages/Certifications';
+import TechnicalSkills from '../pages/TechnicalSkills';
 // Import other page components as needed
-import { useSearchParams } from 'next/navigation';
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const searchParams = useSearchParams();
-  const modalContent = searchParams.get('modal');
+  const [modalContent, setModalContent] = useState(null);
 
-  const openModal = () => setIsModalOpen(true);
-const closeModal = () => setIsModalOpen(false);
+  const openModal = (content) => {
+    setModalContent(content);
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalContent(null);
+  };
 
-const renderModalContent = () => {
-switch (modalContent) {
-case 'contactInfo':
-return <ContactInfo />;
-// Include other cases for each modal content
-default:
-return null;
-}
-};
+  const renderModalContent = () => {
+    switch (modalContent) {
+      case 'ContactInfo':
+        return <ContactInfo />;
+      case 'Objective':
+        return <Objective />;
+      case 'ProfessionalExperience':
+        return <ProfessionalExperience />;
+      case 'Education':
+        return <Education />;
+      case 'Certifications':
+        return <Certifications />;
+      case 'TechnicalSkills':
+        return <TechnicalSkills />;
+      default:
+        return null;
+    }
+  };
 
-return (
-<div>
-<Header />
-<Menu openModal={openModal} />
-{isModalOpen && (
-<Modal onClose={closeModal}>
-{renderModalContent()}
-</Modal>
-)}
-{/* Rest of the page content /}
-<CharacterStats />
-{/ ... other components */}
-</div>
-);
+  return (
+    <div>
+      <Header />
+      <Menu openModal={openModal} />
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          {renderModalContent()}
+        </Modal>
+      )}
+      <CharacterStats />
+      {/* ... other components */}
+    </div>
+  );
 };
--- components/Menu.js
+++ components/Menu.js
@@ -5,12 +5,12 @@ import styles from '/styles/Menu.module.css'; // Make sure the path to your CSS 
 const Menu = ({ openModal }) => {
   return (
     <div className={styles.menu}>
-      <a onClick={() => openModal('ContactInfo')} className={styles.menuItem}>Contact Info</a>
-      <a onClick={() => openModal('Objective')} className={styles.menuItem}>Objective</a>
-      <a onClick={() => openModal('ProfessionalExperience')} className={styles.menuItem}>Professional Experience</a>
-      <a onClick={() => openModal('Education')} className={styles.menuItem}>Education</a>
-      <a onClick={() => openModal('Certifications')} className={styles.menuItem}>Certifications</a>
-      <a onClick={() => openModal('TechnicalSkills')} className={styles.menuItem}>Technical Skills</a>
+      <div onClick={() => openModal('ContactInfo')} className={styles.menuItem}>Contact Info</div>
+      <div onClick={() => openModal('Objective')} className={styles.menuItem}>Objective</div>
+      <div onClick={() => openModal('ProfessionalExperience')} className={styles.menuItem}>Professional Experience</div>
+      <div onClick={() => openModal('Education')} className={styles.menuItem}>Education</div>
+      <div onClick={() => openModal('Certifications')} className={styles.menuItem}>Certifications</div>
+      <div onClick={() => openModal('TechnicalSkills')} className={styles.menuItem}>Technical Skills</div>
       {/* Add any additional links you may have */}
     </div>
   );
