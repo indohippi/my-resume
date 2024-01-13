75ea41e7439a1e2e1e0a861db47f0cbc793750dc
Jared <126293208+indohippi@users.noreply.github.com>
2024-01-13 01:59:13 -0600

    header updates

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,11 @@
-// components/Header.js
 import React from 'react';
 import styles from '/styles/Header.module.css'; // Path to your CSS module
 
 const Header = ({ onClose }) => {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <h1 className={styles.headerTitle}>JARED WIENEN - Web Developer</h1>
-      <button onClick={onClose} className={styles.headerClose}>Close</button>
-    </div>
+    </header>
   );
 };
 
