d8766b758887acf34fed6def3bb335693f4ffd69
Jared <126293208+indohippi@users.noreply.github.com>
2024-01-12 23:59:13 -0600

    updated to CharacterStats hpmp bars

--- components/CharacterStats.js
+++ components/CharacterStats.js
@@ -6,30 +6,29 @@ import styles from '/styles/CharacterStats.module.css';
 const CharacterStats = () => {
   return (
     <div className={styles.characterContainer}>
-      <div className={styles.characterBubble}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="/IMG_1079.JPG" // Correct the path if needed
-            alt="Jared"
-            width={100} // Replace with the intrinsic width of your image
-            height={100} // Replace with the intrinsic height of your image
-          />
+      <Image
+        src="/IMG_1079.JPG" // Correct the path if needed
+        alt="Jared"
+        width={100} // Replace with the intrinsic width of your image
+        height={100} // Replace with the intrinsic height of your image
+        className={styles.characterImage}
+      />
+      <div className={styles.statsContainer}>
+        <div className={styles.statItem}>Jared</div>
+        <div className={styles.statRow}>
+          <span className={styles.statLabel}>LV</span>
+          <span className={styles.statValue}>33</span>
         </div>
-        <div className={styles.stats}>
-          <div className={styles.statItem}>Jared</div>
-          <div className={styles.statRow}>
-            <span className={styles.statLabel}>LV</span>
-            <span className={styles.statValue}>33</span>
-          </div>
-          <div className={styles.statRow}>
-            <span className={styles.statLabel}>HP</span>
-            <span className={styles.statValue}>3000/7777</span>
-          </div>
-          <div className={styles.statRow}>
-            <span className={styles.statLabel}>MP</span>
-            <span className={styles.statValue}>243/400</span>
-          </div>
+        <div className={styles.statRow}>
+          <span className={styles.statLabel}>HP</span>
+          <span className={styles.statValue}>7777/7777</span>
         </div>
+        <div className={styles.hpBar}></div> {/* HP gradient bar */}
+        <div className={styles.statRow}>
+          <span className={styles.statLabel}>MP</span>
+          <span className={styles.statValue}>400/400</span>
+        </div>
+        <div className={styles.mpBar}></div> {/* MP gradient bar */}
       </div>
     </div>
   );
